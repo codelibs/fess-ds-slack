18041ffefa67c420c645cd107c64a4942809a4ba
igarashi <igarashi.kouki@gmail.com>
2019-08-22 17:01:25 +0900

    refactoring

--- src/main/java/org/codelibs/fess/ds/slack/SlackDataStore.java
+++ src/main/java/org/codelibs/fess/ds/slack/SlackDataStore.java
@@ -15,12 +15,15 @@
  */
 package org.codelibs.fess.ds.slack;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.codelibs.core.lang.StringUtil;
@@ -28,16 +31,10 @@ import org.codelibs.fess.crawler.exception.CrawlingAccessException;
 import org.codelibs.fess.ds.AbstractDataStore;
 import org.codelibs.fess.ds.callback.IndexUpdateCallback;
 import org.codelibs.fess.ds.slack.api.SlackClient;
-import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsHistoryResponse;
-import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsListResponse;
-import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsRepliesResponse;
-import org.codelibs.fess.ds.slack.api.method.users.UsersListResponse;
 import org.codelibs.fess.ds.slack.api.type.Attachment;
-import org.codelibs.fess.ds.slack.api.type.Bot;
 import org.codelibs.fess.ds.slack.api.type.Channel;
 import org.codelibs.fess.ds.slack.api.type.Message;
 import org.codelibs.fess.ds.slack.api.type.Team;
-import org.codelibs.fess.ds.slack.api.type.User;
 import org.codelibs.fess.es.config.exentity.DataConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,6 +48,7 @@ public class SlackDataStore extends AbstractDataStore {
     protected static final String CHANNELS_PARAM = "channels";
     protected static final String CHANNELS_ALL = "*all";
     protected static final String CHANNELS_SEPARATOR = ",";
+    protected static final String NUMBER_OF_THREADS = "number_of_threads";
 
     // scripts
     protected static final String MESSAGE = "message";
@@ -61,9 +59,6 @@ public class SlackDataStore extends AbstractDataStore {
     protected static final String MESSAGE_PERMALINK = "permalink";
     protected static final String MESSAGE_ATTACHMENTS = "attachments";
 
-    protected final Map<String, User> usersMap = new HashMap<>();
-    protected final Map<String, Bot> botsMap = new HashMap<>();
-    protected final Map<String, Channel> channelsMap = new HashMap<>();
 
     protected String getName() {
         return this.getClass().getSimpleName();
@@ -78,103 +73,59 @@ public class SlackDataStore extends AbstractDataStore {
             return;
         }
 
-        final SlackClient client = new SlackClient(token);
+        final SlackClient client = new SlackClient(token, paramMap);
         final Team team = client.team.info().execute().getTeam();
-        initUsersMap(client);
-        initChannelsMap(client);
         storeMessages(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team);
     }
 
-    protected void initUsersMap(final SlackClient client) {
-        UsersListResponse response = client.users.list().limit(100).execute();
-        while (true) {
-            if (!response.ok()) {
-                logger.warn("Slack API error occured on \"users.list\": " + response.getError());
-                return;
-            }
-            for (final User user : response.getMembers()) {
-                usersMap.put(user.getId(), user);
-                usersMap.put(user.getName(), user);
-            }
-            final String nextCursor = response.getResponseMetadata().getNextCursor();
-            if (nextCursor.isEmpty()) {
-                break;
-            }
-            response = client.users.list().limit(100).cursor(nextCursor).execute();
+    protected ExecutorService newFixedThreadPool(final int nThreads) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Executor Thread Pool: " + nThreads);
         }
-    }
-
-    protected void initChannelsMap(final SlackClient client) {
-        ConversationsListResponse response = client.conversations.list().limit(100).execute();
-        while (true) {
-            if (!response.ok()) {
-                logger.warn("Slack API error occured on \"conversations.list\": " + response.getError());
-                return;
-            }
-            for (final Channel channel : response.getChannels()) {
-                channelsMap.put(channel.getId(), channel);
-                channelsMap.put(channel.getName(), channel);
-            }
-            final String nextCursor = response.getResponseMetadata().getNextCursor();
-            if (nextCursor.isEmpty()) {
-                break;
-            }
-            response = client.conversations.list().limit(100).cursor(nextCursor).execute();
-        }
+        return new ThreadPoolExecutor(nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(nThreads),
+                new ThreadPoolExecutor.CallerRunsPolicy());
     }
 
     protected void storeMessages(final DataConfig dataConfig, final IndexUpdateCallback callback, final Map<String, String> paramMap,
             final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap, final SlackClient client, final Team team) {
-        for (final Channel channel : getChannels(paramMap)) {
-            processChannelMessages(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel);
+        final ExecutorService executorService = newFixedThreadPool(Integer.parseInt(paramMap.getOrDefault(NUMBER_OF_THREADS, "1")));
+        try {
+            client.getChannels(channel -> {
+                executorService
+                        .execute(() -> processChannelMessages(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel));
+            });
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Shutting down thread executor.");
+            }
+            executorService.shutdown();
+            executorService.awaitTermination(60, TimeUnit.SECONDS);
+        } catch(final InterruptedException e) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Interrupted.", e);
+            }
+        } finally {
+            executorService.shutdownNow();
         }
     }
 
     protected void processChannelMessages(final DataConfig dataConfig, final IndexUpdateCallback callback,
             final Map<String, String> paramMap, final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap,
             final SlackClient client, final Team team, final Channel channel) {
-        ConversationsHistoryResponse response = client.conversations.history(channel.getId()).limit(1000).execute();
-        while (true) {
-            if (!response.ok()) {
-                logger.warn("Slack API error occured on \"conversations.history\": " + response.getError());
-                return;
+        client.getChannelMessages(channel.getId(), message -> {
+            processMessage(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, message);
+            if (message.getThreadTs() != null) {
+                processMessageReplies(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, message);
             }
-            for (final Message message : response.getMessages()) {
-                processMessage(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, message);
-                if (message.getThreadTs() != null) {
-                    processMessageReplies(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, message);
-                }
-            }
-            if (!response.hasMore()) {
-                break;
-            }
-            response = client.conversations.history(channel.getId()).limit(1000).cursor(response.getResponseMetadata().getNextCursor())
-                    .execute();
-        }
+        });
     }
 
     protected void processMessageReplies(final DataConfig dataConfig, final IndexUpdateCallback callback,
             final Map<String, String> paramMap, final Map<String, String> scriptMap, final Map<String, Object> defaultDataMap,
-            final SlackClient client, final Team team, final Channel channel, final Message message) {
-        ConversationsRepliesResponse response = client.conversations.replies(channel.getId(), message.getThreadTs()).limit(1000).execute();
-        while (true) {
-            if (!response.ok()) {
-                logger.warn("Slack API error occured on \"conversations.replies\": " + response.getError());
-                return;
-            }
-            final List<Message> messages = response.getMessages();
-            for (int i = 1; i < messages.size(); i++) {
-                if (message.isThreadBroadcast()) {
-                    continue;
-                }
-                processMessage(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, messages.get(i));
-            }
-            if (!response.hasMore()) {
-                break;
-            }
-            response = client.conversations.replies(channel.getId(), message.getThreadTs()).limit(1000)
-                    .cursor(response.getResponseMetadata().getNextCursor()).execute();
-        }
+            final SlackClient client, final Team team, final Channel channel, final Message parentMessage) {
+        client.getMessageReplies(channel.getId(), parentMessage.getThreadTs(), message -> {
+                processMessage(dataConfig, callback, paramMap, scriptMap, defaultDataMap, client, team, channel, message);
+            });
     }
 
     protected void processMessage(final DataConfig dataConfig, final IndexUpdateCallback callback, final Map<String, String> paramMap,
@@ -187,11 +138,11 @@ public class SlackDataStore extends AbstractDataStore {
         final Map<String, Object> messageMap = new HashMap<>();
 
         try {
-            messageMap.put(MESSAGE_TEXT, getMessageText(client, message));
+            messageMap.put(MESSAGE_TEXT, getMessageText(message));
             messageMap.put(MESSAGE_TIMESTAMP, getMessageTimestamp(message));
-            messageMap.put(MESSAGE_USER, getMessageUser(client, message));
+            messageMap.put(MESSAGE_USER, client.getMessageUsername(message));
             messageMap.put(MESSAGE_CHANNEL, channel.getName());
-            messageMap.put(MESSAGE_PERMALINK, getMessagePermalink(client, team, channel, message));
+            messageMap.put(MESSAGE_PERMALINK, client.getMessagePermalink(team, channel, message));
             messageMap.put(MESSAGE_ATTACHMENTS, getMessageAttachmentsText(message));
             resultMap.put(MESSAGE, messageMap);
 
@@ -207,7 +158,7 @@ public class SlackDataStore extends AbstractDataStore {
         }
     }
 
-    protected String getMessageText(final SlackClient client, final Message message) {
+    protected String getMessageText(final Message message) {
         final String text = message.getText();
         return text != null ? text : "";
     }
@@ -216,51 +167,12 @@ public class SlackDataStore extends AbstractDataStore {
         return new Date(Math.round(Double.parseDouble(message.getTs()) * 1000));
     }
 
-    protected String getMessageUser(final SlackClient client, final Message message) {
-        if (message.getUsername() != null) {
-            return message.getUsername();
-        }
-        if (message.getSubtype() != null) {
-            if (message.getSubtype().equals("bot_message")) {
-                Bot bot = botsMap.get(message.getBotId());
-                if (bot == null) {
-                    botsMap.put(message.getBotId(), bot = client.bots.info().bot(message.getBotId()).execute().getBot());
-                }
-                return bot.getName();
-            } else if (message.getSubtype().equals("file_comment")) {
-                User user = usersMap.get(message.getComment().getUser());
-                if (user == null) {
-                    usersMap.put(message.getUser(), user = client.users.info(message.getComment().getUser()).execute().getUser());
-                }
-                return !user.getProfile().getDisplayName().isEmpty() ? user.getProfile().getDisplayName() : user.getProfile().getRealName();
-            }
-        }
-        User user = usersMap.get(message.getUser());
-        if (user == null) {
-            usersMap.put(message.getUser(), user = client.users.info(message.getUser()).execute().getUser());
-        }
-        return !user.getProfile().getDisplayName().isEmpty() ? user.getProfile().getDisplayName() : user.getProfile().getRealName();
-    }
-
-    protected String getMessagePermalink(final SlackClient client, final Team team, final Channel channel, final Message message) {
-        String permalink = message.getPermalink();
-        if (permalink == null) {
-            if (team == null) {
-                permalink = client.chat.getPermalink(channel.getId(), message.getTs()).execute().getPermalink();
-            } else {
-                permalink =
-                        "https://" + team.getDomain() + ".slack.com/archives/" + channel.getId() + "/p" + message.getTs().replace(".", "");
-            }
-        }
-        return permalink;
-    }
-
     protected String getMessageAttachmentsText(final Message message) {
         final List<Attachment> attachments = message.getAttachments();
         if (attachments == null) {
             return "";
         }
-        final List<String> fallbacks = attachments.stream().map(attachment -> attachment.getFallback()).collect(Collectors.toList());
+        final List<String> fallbacks = attachments.stream().map(Attachment::getFallback).collect(Collectors.toList());
         return String.join("\n", fallbacks);
     }
 
@@ -269,20 +181,6 @@ public class SlackDataStore extends AbstractDataStore {
             return paramMap.get(TOKEN_PARAM);
         }
         return StringUtil.EMPTY;
-    }
-
-    protected List<Channel> getChannels(final Map<String, String> paramMap) {
-        final List<Channel> channels = new ArrayList<>();
-        if (!paramMap.containsKey(CHANNELS_PARAM) || paramMap.get(CHANNELS_PARAM).equals(CHANNELS_ALL)) {
-            channels.addAll(channelsMap.values());
-        } else {
-            for (final String name : paramMap.get(CHANNELS_PARAM).split(CHANNELS_SEPARATOR)) {
-                if (channelsMap.containsKey(name)) {
-                    channels.add(channelsMap.get(name));
-                }
-            }
-        }
-        return channels;
     }
 
 }
--- src/main/java/org/codelibs/fess/ds/slack/api/SlackClient.java
+++ src/main/java/org/codelibs/fess/ds/slack/api/SlackClient.java
@@ -15,19 +15,47 @@
  */
 package org.codelibs.fess.ds.slack.api;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.codelibs.core.lang.StringUtil;
 import org.codelibs.curl.CurlRequest;
 import org.codelibs.fess.ds.slack.api.method.bots.BotsClient;
 import org.codelibs.fess.ds.slack.api.method.chat.ChatClient;
 import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsClient;
+import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsHistoryResponse;
+import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsListResponse;
+import org.codelibs.fess.ds.slack.api.method.conversations.ConversationsRepliesResponse;
 import org.codelibs.fess.ds.slack.api.method.files.FilesClient;
 import org.codelibs.fess.ds.slack.api.method.team.TeamClient;
 import org.codelibs.fess.ds.slack.api.method.users.UsersClient;
+import org.codelibs.fess.ds.slack.api.method.users.UsersListResponse;
+import org.codelibs.fess.ds.slack.api.type.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SlackClient {
 
-    public static final String SLACK_API_ENDPOINT = "https://slack.com/api/";
+    private static final Logger logger = LoggerFactory.getLogger(SlackClient.class);
+
+    protected static final String SLACK_API_ENDPOINT = "https://slack.com/api/";
+
+    protected static final String TOKEN_PARAM = "token";
+    protected static final String CHANNELS_PARAM = "channels";
+    protected static final String CHANNELS_ALL = "*all";
+    protected static final String CHANNELS_SEPARATOR = ",";
+
+    protected static final String USER_CACHE_SIZE = "user_cache_size";
+    protected static final String BOT_CACHE_SIZE = "bot_cache_size";
+    protected static final String CHANNEL_CACHE_SIZE = "channel_cache_size";
+
+    // TODO : 最大フェッチ数を種類ごとに分ける。
+    protected static final Integer DEFAULT_FETCH_LIMIT = 100;
 
     protected final String token;
 
@@ -37,8 +65,14 @@ public class SlackClient {
     public final BotsClient bots;
     public final ChatClient chat;
     public final TeamClient team;
+    protected Map<String, String> params;
+    protected LoadingCache<String, User> usersCache;
+    protected LoadingCache<String, Bot> botsCache;
+    protected LoadingCache<String, Channel> channelsCache;
 
-    public SlackClient(final String token) {
+    // TODO token is not needed?
+    public SlackClient(final String token, final Map<String, String> params) {
+        this.params = params;
         this.token = token;
         this.conversations = new ConversationsClient(this);
         this.users = new UsersClient(this);
@@ -46,6 +80,46 @@ public class SlackClient {
         this.bots = new BotsClient(this);
         this.chat = new ChatClient(this);
         this.team = new TeamClient(this);
+        // TODO
+        usersCache = CacheBuilder
+                .newBuilder()
+                .maximumSize(Integer.parseInt(params.getOrDefault(USER_CACHE_SIZE, "10000")))
+                .build(new CacheLoader<String, User>() {
+                        @Override
+                        public User load(final String key) {
+                            return users.info(key).execute().getUser();
+                        }
+                    }
+                );
+        botsCache = CacheBuilder
+                .newBuilder()
+                .maximumSize(Integer.parseInt(params.getOrDefault(BOT_CACHE_SIZE, "10000")))
+                .build(new CacheLoader<String, Bot>() {
+                           @Override
+                           public Bot load(final String key) {
+                               return bots.info().bot(key).execute().getBot();
+                           }
+                       }
+                );
+        channelsCache = CacheBuilder
+                .newBuilder()
+                .maximumSize(Integer.parseInt(params.getOrDefault(CHANNEL_CACHE_SIZE, "10000")))
+                .build(new CacheLoader<String, Channel>() {
+                           @Override
+                           public Channel load(final String key) {
+                               return conversations.info(key).execute().getChannel();
+                           }
+                       }
+                );
+        // TODO : Initilization
+        getUsers( user -> {
+            usersCache.put(user.getId(), user);
+            usersCache.put(user.getName(), user);
+        });
+        getAllChannels( channel -> {
+            channelsCache.put(channel.getId(), channel);
+            channelsCache.put(channel.getName(), channel);
+        });
     }
 
     public CurlRequest request(final Function<String, CurlRequest> method, final String path) {
@@ -56,5 +130,155 @@ public class SlackClient {
         }
         return method.apply(buf.toString()).header("Authorization", "Bearer " + token);
     }
+
+    public void getChannels(final Consumer<Channel> consumer) {
+        if (!params.containsKey(CHANNELS_PARAM) || params.get(CHANNELS_PARAM).equals(CHANNELS_ALL)) {
+            getAllChannels(consumer);
+        } else {
+            for (final String name : params.get(CHANNELS_PARAM).split(CHANNELS_SEPARATOR)) {
+                try {
+                    final Channel channel = channelsCache.get(name);
+                    if(channel != null) consumer.accept(channel);
+                } catch (Exception e) {
+                    // TODO
+                    logger.warn("Failed to get a Channel objet.", e);
+                }
+            }
+        }
+    }
+
+    public void getAllChannels(final Consumer<Channel> consumer) {
+        getAllChannels(DEFAULT_FETCH_LIMIT, consumer);
+    }
+
+    public void getAllChannels(final Integer limit, final Consumer<Channel> consumer) {
+        ConversationsListResponse response = conversations.list().limit(limit).execute();
+        while (true) {
+            if (!response.ok()) {
+                logger.warn("Slack API error occured on \"conversations.list\": " + response.getError());
+                return;
+            }
+            response.getChannels().forEach(consumer);
+            final String nextCursor = response.getResponseMetadata().getNextCursor();
+            if (nextCursor.isEmpty()) {
+                break;
+            }
+            response = conversations.list().limit(limit).cursor(nextCursor).execute();
+        }
+    }
+
+    public void getChannelMessages(final String channelId, final Consumer<Message> consumer) {
+        getChannelMessages(channelId, DEFAULT_FETCH_LIMIT, consumer);
+    }
+
+    public void getChannelMessages(final String channelId, final Integer limit, final Consumer<Message> consumer) {
+        ConversationsHistoryResponse response = conversations.history(channelId).limit(limit).execute();
+        while (true) {
+            if (!response.ok()) {
+                logger.warn("Slack API error occured on \"conversations.history\": " + response.getError());
+                return;
+            }
+            response.getMessages().forEach(consumer);
+            if (!response.hasMore()) {
+                break;
+            }
+            response = conversations.history(channelId).limit(limit).cursor(response.getResponseMetadata().getNextCursor())
+                    .execute();
+        }
+    }
+
+    public void getMessageReplies(final String channelId, final String threadTs, final Consumer<Message> consumer) {
+        getMessageReplies(channelId, threadTs, DEFAULT_FETCH_LIMIT, consumer);
+    }
+
+    public void getMessageReplies(final String channelId, final String threadTs, final Integer limit, final Consumer<Message> consumer) {
+        ConversationsRepliesResponse response = conversations.replies(channelId, threadTs).limit(limit).execute();
+        while (true) {
+            if (!response.ok()) {
+                logger.warn("Slack API error occured on \"conversations.replies\": " + response.getError());
+                return;
+            }
+            final List<Message> messages = response.getMessages();
+            for (int i = 1; i < messages.size(); i++) {
+                final Message message = messages.get(i);
+                if (message.isThreadBroadcast()) {
+                    continue;
+                }
+                consumer.accept(message);
+            }
+            if (!response.hasMore()) {
+                break;
+            }
+            response = conversations.replies(channelId, threadTs).limit(limit)
+                    .cursor(response.getResponseMetadata().getNextCursor()).execute();
+        }
+    }
+
+    public void getUsers(final Consumer<User> consumer) {
+        getUsers(DEFAULT_FETCH_LIMIT, consumer);
+    }
+
+    public void getUsers(final Integer limit, final Consumer<User> consumer) {
+        UsersListResponse response = users.list().limit(limit).execute();
+        while (true) {
+            if (!response.ok()) {
+                logger.warn("Slack API error occured on \"users.list\": " + response.getError());
+                return;
+            }
+            response.getMembers().forEach(consumer);
+            final String nextCursor = response.getResponseMetadata().getNextCursor();
+            if (nextCursor.isEmpty()) {
+                break;
+            }
+            response = users.list().limit(limit).cursor(nextCursor).execute();
+        }
+    }
+
+    public String getMessageUsername(final Message message) {
+        if (message.getUsername() != null) {
+            return message.getUsername();
+        }
+        if (message.getSubtype() != null) {
+            if (message.getSubtype().equals("bot_message")) {
+                try {
+                    final Bot bot = botsCache.get(message.getBotId());
+                    return bot.getName();
+                } catch (final Exception e) {
+                    // TODO
+                    logger.warn("Failed to get a bot name.", e);
+                }
+            } else if (message.getSubtype().equals("file_comment")) {
+                try {
+                    final User user = usersCache.get(message.getComment().getUser());
+                    return !user.getProfile().getDisplayName().isEmpty() ? user.getProfile().getDisplayName() : user.getProfile().getRealName();
+                } catch (final Exception e) {
+                    // TODO
+                    logger.warn("Failed to get a user name.", e);
+                }
+            }
+        }
+        try {
+            final User user = usersCache.get(message.getUser());
+            return !user.getProfile().getDisplayName().isEmpty() ? user.getProfile().getDisplayName() : user.getProfile().getRealName();
+        } catch (final Exception e) {
+            // TODO
+            logger.warn("Failed to get a user name.", e);
+        }
+        return StringUtil.EMPTY;
+    }
+
+    public String getMessagePermalink(final Team team, final Channel channel, final Message message) {
+        String permalink = message.getPermalink();
+        if (permalink == null) {
+            if (team == null) {
+                permalink = chat.getPermalink(channel.getId(), message.getTs()).execute().getPermalink();
+            } else {
+                permalink =
+                        "https://" + team.getDomain() + ".slack.com/archives/" + channel.getId() + "/p" + message.getTs().replace(".", "");
+            }
+        }
+        return permalink;
+    }
+
 
 }
